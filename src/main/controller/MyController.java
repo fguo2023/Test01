dd535fa9beea55790d77a5778e84791b5144c9df
fguo2023 <fguo520@gmail.com>
2023-02-25 14:46:23 -0800

    add lock

--- src/main/controller/MyController.java
+++ src/main/controller/MyController.java
@@ -2,8 +2,16 @@ package main.controller;
 
 import org.openjdk.jol.info.ClassLayout;
 
+import java.util.concurrent.locks.ReentrantLock;
+
 public class MyController {
     public static void main(String[] args) {
-
+        ReentrantLock lock = new ReentrantLock();
+        lock.lock();
+        try{
+            // your code
+        }finally {
+            lock.unlock();
+        }
     }
 }
